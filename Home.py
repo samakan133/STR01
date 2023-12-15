7aac29443cd131a8f6446a31fbabc3dcf6577d0e
samakan133 <96411028+samakan133@users.noreply.github.com>
2023-12-15 15:56:33 +0700

    .commit

--- Home.py
+++ Home.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 st.header("Samakan Panyaroj")
 
+st.balloons()
 st.title('การทดสอบเขียนเว็บด้วย Python')
 st.header('นาย สมกานต์ ปัญญาโรจน์')
 st.subheader('สาขาวิชาเทคโนโลยีสารสนเทศ')

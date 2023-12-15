dd409b497535ab7d4f022896eed0e485945b8c82
samakan133 <96411028+samakan133@users.noreply.github.com>
2023-12-15 14:20:43 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
 st.header("Samakan Panyaroj")
+
+st.title('การทดสอบเขียนเว็บด้วย Python')
+st.header('ผศ.ดร.ไก้รุ่ง เฮงพระพรหม')
+st.subheader('สาขาวิชาวิทยาการข้อมูล')
+st.markdown("----")

fbcbf675185b48cb39b793c95a7fde3c8c290326
samakan133 <96411028+samakan133@users.noreply.github.com>
2023-12-15 14:30:52 +0700

    .commit

--- Home.py
+++ Home.py
@@ -6,3 +6,11 @@ st.title('การทดสอบเขียนเว็บด้วย Python
 st.header('นาย สมกานต์ ปัญญาโรจน์')
 st.subheader('สาขาวิชาเทคโนโลยีสารสนเทศ')
 st.markdown("----")
+
+col1, col2 = st.columns(2)
+#col1.write("This is column 1")
+#col2.write("This is column 2")
+with col1:
+    st.image('./pic/samakan.jpg')
+with col2:
+    st.image('./pic/ทานตะวัน.jpg')
